d851ba563b8a46f368f76ebd19e1ae40a7a869a4
p00ria <amir1300sobatdar@gmail.com>
2016-11-01 17:28:47 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+TOKEN = 'Your Token Here'
+DEEP_LOGGING = False
+Sudo = 142141024
+Admins = (142141024, 0)
+REPLIER = True
+GP_GREETING = True
+GP_FAREWELL = True
+
+}
+
+enabled_plugins = [
+"Banhammer",
+"Broadcast",
+"Calculator",
+"Echocho",
+"Feedback",
+"ID",
+"Stats",
+"Message",
+"Start",
+"Download",
+"Callback",
+"Inline",
+"Shortlink",
+]
